3e677a3ecab401ff3b5b1bad3749a01d5c13b3c1
Joshua Moody <joshuajmoody@gmail.com>
2015-08-02 14:48:06 +0200

    Finalize the make app-cal script

--- Scripts/make/make-app-cal.sh
+++ Scripts/make/make-app-cal.sh
@@ -2,34 +2,38 @@
 
 bundle
 
-FIRST_ARG="${1}"
+make keychain
+KEYCHAIN="${HOME}/.calabash/calabash-codesign/ios/Calabash.keychain"
+IDENTITY=`xcrun security find-identity -v -p codesigning ${KEYCHAIN} | awk 'match($0, /\"iPhone Developer: .+\"/) { print substr($0, RSTART, RLENGTH)}' | tr -d '\n'`
 
-if [ "${FIRST_ARG}" = "calabash" ]; then
-  TARGET_NAME="RiseUp-cal"
-else
-  TARGET_NAME="RiseUp"
-fi
+WORKSPACE="WordPress.xcworkspace"
+SCHEME="WordPress"
+TARGET_NAME="WordPress"
+CONFIG=Calabash
 
-XC_PROJECT="RiseUp.xcodeproj"
-XC_SCHEME="${TARGET_NAME}"
-CAL_BUILD_CONFIG="Debug"
+CAL_BUILD_DIR="${PWD}/build"
 
-CAL_BUILD_DIR="${PWD}/build/simulator"
 rm -rf "${CAL_BUILD_DIR}"
 mkdir -p "${CAL_BUILD_DIR}"
+
+PRODUCT_DIR=./Calabash-app
+rm -rf ${PRODUCT_DIR}
+mkdir -p ${PRODUCT_DIR}
 
 set +o errexit
 
 xcrun xcodebuild \
+  CODE_SIGN_IDENTITY="${IDENTITY}" \
+  OTHER_CODE_SIGN_FLAGS="--keychain ${KEYCHAIN}" \
   -SYMROOT="${CAL_BUILD_DIR}" \
   -derivedDataPath "${CAL_BUILD_DIR}" \
+  -workspace "${WORKSPACE}" \
+  -scheme "${SCHEME}" \
+  -configuration "${CONFIG}" \
   ARCHS="i386 x86_64" \
   VALID_ARCHS="i386 x86_64" \
   ONLY_ACTIVE_ARCH=NO \
-  -project "${XC_PROJECT}" \
-  -scheme "${TARGET_NAME}" \
   -sdk iphonesimulator \
-  -configuration "${CAL_BUILD_CONFIG}" \
   clean build | xcpretty -c
 
 RETVAL=${PIPESTATUS[0]}
@@ -43,13 +47,10 @@ else
   echo "INFO: successfully built"
 fi
 
-rm -rf "${PWD}/${TARGET_NAME}.app"
+APP_BUNDLE_PATH="${CAL_BUILD_DIR}/Build/Products/${CONFIG}-iphonesimulator/${TARGET_NAME}.app"
+mv "${APP_BUNDLE_PATH}" "${PRODUCT_DIR}"
+echo "INFO: Created ${PRODUCT_DIR}/${TARGET_NAME}.app"
 
-APP_BUNDLE_PATH="${CAL_BUILD_DIR}/Build/Products/${CAL_BUILD_CONFIG}-iphonesimulator/${TARGET_NAME}.app"
-cp -r "${APP_BUNDLE_PATH}" "${PWD}"
-
-echo "INFO: Created ${TARGET_NAME}.app"
-
-echo "INFO: Cleaning stale targets"
-bundle exec run-loop simctl install --app "${PWD}/${TARGET_NAME}.app" --debug
-
+DYSM_PATH="${CAL_BUILD_DIR}/Build/Products/${CONFIG}-iphonesimulator/${TARGET_NAME}.app.dSYM"
+mv "${DYSM_PATH}" "${PRODUCT_DIR}"
+echo "INFO: Created ${PRODUCT_DIR}/${TARGET_NAME}.app.dSYM"
